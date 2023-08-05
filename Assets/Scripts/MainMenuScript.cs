fbdd78137fc100924d5c43b1ec61308d1357b5bf
Limmerko <limmerkogames@gmail.com>
2023-08-05 15:56:45 +0300

    Инициализация вибрации

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainMenuScriprt : MonoBehaviour
+public class MainMenuScript : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
+        Vibration.Init();
+        
         if (!PlayerPrefs.HasKey("Level"))
         {
             PlayerPrefs.SetInt("Level", 0);
